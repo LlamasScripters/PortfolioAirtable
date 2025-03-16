c4e9c223ac34f8235f86b699a9793f37982ddd3c
AriaAman <ariaaman@outlook.fr>
2025-03-16 23:58:01 +0100

    Update du titre de l'app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,12 +24,12 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en', // App language
       },
-      title: siteTitle, // App window nav title
+      title: "Portfolio", // App window nav title
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'A awesome developer portfolio design.' },
-        { hid: 'og:title', property: 'og:title', content: siteTitle },
+        { hid: 'og:title', property: 'og:title', content: "Portfolio" },
         { hid: 'og:description', property: 'og:description', content: 'A awesome developer portfolio design.' },
         { hid: 'og:image', property: 'og:image', content: 'demo-share.jpg' },
         { hid: 'og:url', property: 'og:url', content: 'https://developer-portfolio-v1.netlify.app/' },
